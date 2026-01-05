90952ff1fd106635a2f88a8123de7bb9dc435550
gantengfei <gan_tengfei@163.com>
2026-01-05 14:09:30 +0800

    update

--- src/js/tongji.js
+++
@@ -1,8 +0,0 @@
-// 统计代码获取
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?8923e3fc09ec1961bae76cb4683ac93a";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
