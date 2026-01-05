350f39f47c543fbac648f26898c6b3a44848ee8d
gantengfei <gan_tengfei@163.com>
2026-01-05 14:06:36 +0800

    add tongji

---
+++ src/js/tongji.js
@@ -0,0 +1,8 @@
+// 统计代码获取
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8923e3fc09ec1961bae76cb4683ac93a";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
