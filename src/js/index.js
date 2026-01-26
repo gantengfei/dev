6e3633c742ef76fb5078fefd9bba946163361437
gantengfei <gan_tengfei@163.com>
2026-01-26 16:57:02 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -164,6 +164,10 @@ $(function () {
     $('html,body').animate({ scrollTop: '0px' }, 500)
   })
 
+
+  $('.head-box .day').on('click', () => {
+    window.open('/src/views/rili/')
+  })
 })
 
 const week = ['日', '一', '二', '三', '四', '五', '六']
@@ -179,6 +183,15 @@ function loadmd(filename, path) {
     loadMarkedMenu();
 
     imgAmplifier();
+
+    let bigimgDom = $('#bigimg')
+    mousewheel(bigimgDom[0], function () {
+      let zoomWidth = bigimgDom.innerWidth() * 1.02;
+      bigimgDom.css({ width: zoomWidth + "px" })
+    }, function () {
+      let zoomWidth = bigimgDom.innerWidth() / 1.02;
+      bigimgDom.css({ width: zoomWidth + "px" })
+    })
   })
 }
 
@@ -227,42 +240,39 @@ function imgAmplifier() {
   $("img").click(function () {
     let _this = $(this);//将当前的img元素作为_this传入函数
     if (_this.attr('id') != 'bigimg') {
-      imgShow("#outerdiv", "#innerdiv", "#bigimg", _this);
+      imgShow("#outerdiv", "#bigimg", _this);
     }
   });
 }
 
-function imgShow(outerdiv, innerdiv, bigimg, _this) {
-  let src = _this.attr("src");//获取当前点击的pimg元素中的src属性
-  $(bigimg).attr("src", src);//设置#bigimg元素的src属性
+function imgShow(outerdiv, bigimg, _this) {
+  let src = _this.attr("src"); // 获取当前点击的pimg元素中的src属性
+  $(bigimg).attr("src", src); // 设置#bigimg元素的src属性
   /*获取当前点击图片的真实大小，并显示弹出层及大图*/
   $("<img/>").attr("src", src).load(function () {
-    let windowW = $(window).width();//获取当前窗口宽度
-    let windowH = $(window).height();//获取当前窗口高度
-    let realWidth = this.width;//获取图片真实宽度
-    let realHeight = this.height;//获取图片真实高度
+    let windowW = $(window).width(); // 获取当前窗口宽度
+    let windowH = $(window).height(); // 获取当前窗口高度
+    let realWidth = this.width; // 获取图片真实宽度
+    let realHeight = this.height; // 获取图片真实高度
     let imgWidth, imgHeight;
-    let scale = 0.9;//缩放尺寸，当图片真实宽度和高度大于窗口宽度和高度时进行缩放
-    if (realHeight > windowH * scale) {//判断图片高度
-      imgHeight = windowH * scale;//如大于窗口高度，图片高度进行缩放
-      imgWidth = imgHeight / realHeight * realWidth;//等比例缩放宽度
-      if (imgWidth > windowW * scale) {//如宽度扔大于窗口宽度
-        imgWidth = windowW * scale;//再对宽度进行缩放
+    let scale = 0.9; // 缩放尺寸，当图片真实宽度和高度大于窗口宽度和高度时进行缩放
+    if (realHeight > windowH * scale) { // 判断图片高度
+      imgHeight = windowH * scale; // 如大于窗口高度，图片高度进行缩放
+      imgWidth = imgHeight / realHeight * realWidth; // 等比例缩放宽度
+      if (imgWidth > windowW * scale) { // 如宽度扔大于窗口宽度
+        imgWidth = windowW * scale; // 再对宽度进行缩放
       }
-    } else if (realWidth > windowW * scale) {//如图片高度合适，判断图片宽度
-      imgWidth = windowW * scale;//如大于窗口宽度，图片宽度进行缩放
-      imgHeight = imgWidth / realWidth * realHeight;//等比例缩放高度
-    } else {//如果图片真实高度和宽度都符合要求，高宽不变
+    } else if (realWidth > windowW * scale) { // 如图片高度合适，判断图片宽度
+      imgWidth = windowW * scale; // 如大于窗口宽度，图片宽度进行缩放
+      imgHeight = imgWidth / realWidth * realHeight; // 等比例缩放高度
+    } else { // 如果图片真实高度和宽度都符合要求，高宽不变
       imgWidth = realWidth;
       imgHeight = realHeight;
     }
-    $(bigimg).css("width", imgWidth);//以最终的宽度对图片缩放
-    let w = (windowW - imgWidth) / 2;//计算图片与窗口左边距
-    let h = (windowH - imgHeight) / 2;//计算图片与窗口上边距
-    $(innerdiv).css({ "top": h, "left": w });//设置#innerdiv的top和left属性
-    $(outerdiv).fadeIn("fast");//淡入显示#outerdiv及.pimg
+    $(bigimg).css("width", imgWidth); // 以最终的宽度对图片缩放
+    $(outerdiv).fadeIn("fast"); // 淡入显示#outerdiv及.pimg
   });
-  $(outerdiv).click(function () {//再次点击淡出消失弹出层
+  $(outerdiv).click(function () { // 再次点击淡出消失弹出层
     $(this).fadeOut("fast");
   });
 }
@@ -318,4 +328,32 @@ function updateProgressBar() {
 
   // 更新进度条宽度
   $('.progress-bar').width(`${percent}%`);
+}
+
+
+// 发大图片滚轮缩放
+function mousewheel(obj, upfun, downfun) {
+  if (document.attachEvent) {
+    obj.attachEvent("onmousewheel", scrollFn)
+  } else {
+    if (document.addEventListener) {
+      obj.addEventListener("mousewheel", scrollFn, false);
+      obj.addEventListener("DOMMouseScroll", scrollFn, false)
+    }
+  }
+
+  function scrollFn(e) {
+    var ev = e || window.event;
+    var dir = ev.wheelDelta || ev.detail;
+    if (ev.preventDefault) {
+      ev.preventDefault()
+    } else {
+      ev.returnValue = false
+    }
+    if (dir == -3 || dir == 120) {
+      upfun()
+    } else {
+      downfun()
+    }
+  }
 }
